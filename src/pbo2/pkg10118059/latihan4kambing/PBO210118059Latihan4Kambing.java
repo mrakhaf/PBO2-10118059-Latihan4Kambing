71f48df7b6602e5d22a6dd65016a57d6fbaacb74
mrakhaf <48128093+mrakhaf@users.noreply.github.com>
2019-09-26 17:04:56 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118059/latihan4kambing/PBO210118059Latihan4Kambing.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118059.latihan4kambing;
+
+/**
+ *
+ * @author
+ * Nama  : Muhammad Rakha Firdaus
+ * NIM   : 10118059
+ * Kelas : IF-2
+ * 
+ */
+public class PBO210118059Latihan4Kambing {
+    
+    public void tambahKambing(){
+
+        //deklarasi variabel lokal 
+        int jumlahKambing = 0;
+    
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah : " + jumlahKambing);
+    
+    }    
+    public static void main(String[] args) {
+        
+        PBO210118059Latihan4Kambing kambingJantan = new PBO210118059Latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
